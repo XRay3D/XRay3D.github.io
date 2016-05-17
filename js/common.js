46cdbc80b031b81dadaded7475fbee8c0dc3cc7b
user <user>
2016-05-17 16:23:05 +0300

    dwd

--- js/common.js
+++ js/common.js
@@ -7,7 +7,23 @@ $(function() {
        // Optional parameters
        direction: 'horizontal',
        loop: true,
-			 slidesPerView: 1
+			 breakpoints: {
+    // when window width is <= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetweenSlides: 10
+    },
+    // when window width is <= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetweenSlides: 20
+    },
+    // when window width is <= 640px
+    640: {
+      slidesPerView: 3,
+      spaceBetweenSlides: 30
+    }
+  }
      })
    });
 
